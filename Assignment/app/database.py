b4b0341b5e4d8b9e6537e20f4cc0c13a08b3fd47
Al-Meqdad <75421792+Al-Meqdad@users.noreply.github.com>
2021-10-11 19:13:27 +0300

    Add files via upload
    This is the final code with everything ready and fully tested

--- Assignment/app/database.py
+++ Assignment/app/database.py
@@ -1,30 +1,120 @@
-import random
 import uuid
 
-todo_list = [{"id": 54,
-              "description": "Hello World",
-              "status": 0}]
+todo_list = []
 
 
 def insert_data(Description: str) -> None:
+    """[A function that is responsible for inserting data into our list by
+    adding a new descrption and generating a unieq
+    id for each item in that list using the library uuid]
 
+    Args:
+    takes the paremeter desciption as an
+    str and inserted to the list
+    with the uniqe id and fixed value of status which is 0
+    """
     todo_list.append({
-        "id": uuid.uuid4(),
+        "id": str(uuid.uuid4()),
         "description": Description,
         "status": 0
     })
 
 
-def update_data(Data_ID: int, Description: str, status: int) -> dict:
+def update_data(Data_ID, Description: str) -> None:
+    """[A function that is responsible for Updating The data By ID
+    it takes the ID from the api as an argument beisde the Description
+    and search the list for that id and if found it will update
+    the data with no problems
+    if ID was not found it will thros an Exception]
+
+    Exception:
+    The Id entered was not found please try again
+
+    Args:
+    Data_ID: which is the id the function will
+    use to update the data by searching
+    for the data that matches the id
+
+    Description: Which is the new data that
+    will be entered by the user and replace the old description
+    """
     for Data in todo_list:
-        if Data["id"] == Data_ID:
+        if Data['id'] == Data_ID:
             Data["description"] = Description
-            Data["status"] = status
             return
     raise Exception("ID was not found please retry again")
 
 
 def Get_Data() -> dict:
+    """[A function that will get all the data from our list
+    if the list was empty it will raise an Exception]
+
+    Exception:
+    No data was found. *List is empty*
+
+    Returns:
+    todo_list: all the data stored in our list
+    """
+    if len(todo_list) == 0:
+        raise Exception("No data was found. *List is empty*")
+    return todo_list
+
+
+def DeleteDataByID(Data_ID) -> None:
+    """[A function that deletes a specific row
+    from the data by the id and raise an exception if that id was not found]
+
+    Exception:
+    ID was not found please retry again
+
+    Args:
+    Data_ID: the id we use to search the list for the row we want to delete
+    """
+    for index, Data in enumerate(todo_list):
+        if Data['id'] == Data_ID:
+            print(index)
+            todo_list.pop(index)
+            return
+    raise Exception("ID was not found please retry again")
+
+
+def update_data_status(Data_ID, status: int) -> None:
+    """[A function that updates the status of specific row
+    in our data using the id of that row
+    raise an expetion if the id was not found]
+
+    Exception:
+    ID was not found please retry again
+
+    Args:
+    Data_ID: which is the id the function will
+    use to update the status by searching
+    for the data that matches the id
+
+    status: which is the new status that will be updated
+    once the id is found inside the list
+    """
+    for Data in todo_list:
+        if Data['id'] == Data_ID:
+            Data["status"] = status
+            return
+    raise Exception("ID was not found please retry again")
+
+
+def GetDataByID(Data_ID) -> dict:
+    """[A function that is used to get a specifi row from our list of
+    data by using the id raises an Exception if the id was not found]
+
+    Exception:
+    ID was not found please retry again
+
+    Args:
+    Data_ID: the id we use to search the list for the row we want to retreive aka Get
+    """
     if len(todo_list) == 0:
         raise Exception("No data inside the list")
-    return todo_list
+    else:
+        for Data in todo_list:
+            if Data['id'] == Data_ID:
+                return Data
+        raise Exception("ID was not found please retry again")
--- Assignment/app/routes.py
+++ Assignment/app/routes.py
@@ -3,10 +3,19 @@ from app import app
 from app import database as db_helper
 
 
-"""@app.route("/delete/<int:task_id>", methods=['DELETE'])
-def delete(task_id):
+@app.route("/api/v1/todo/<Data_ID>", methods=['DELETE'])
+def delete(Data_ID):
+    """[This api is responsible for deleting a
+    segnificent row from Our list of data by using the id]
+
+    Returns:
+        [json dictionary]: [It reutrn the result of
+        this api method wether it's a success by
+        returning status 200 and success message
+        or failuer by returning status 404 and an error message]
+    """
     try:
-        db_helper.remove_task_by_id(task_id)
+        db_helper.DeleteDataByID(Data_ID)
         result = {
             'status': 200,
             'response': 'Removed task'
@@ -18,18 +27,55 @@ def delete(task_id):
             'response': str(Err)
         }
 
-    return jsonify(result)"""
+    return jsonify(result)
 
 
 @app.route("/api/v1/todo/<Data_ID>", methods=['PUT'])
 def update(Data_ID):
-    Test = db_helper.Get_Data()
+    """[This api is responsible for updating the documant of a
+    segnificent row from Our list of data by using the id]
+
+    Returns:
+        [json dictionary]: [It reutrn the result of
+        this api method wether it's a success by
+        returning status 200 and success message
+        or failuer by returning status 404 and an error message]
+    """
+
     try:
         data = request.get_json()
-        db_helper.update_data(Data_ID, data["description"], data["status"])
+        db_helper.update_data(Data_ID, data["description"])
         result = {
-            "success": 200,
+            "status": 200,
             "response": "Data was updated succefully"
+        }
+    except Exception as err:
+        result = {
+            'status': 404,
+            'response': str(err)
+        }
+
+    return jsonify(result)
+
+
+@app.route("/api/v1/todo/<Data_ID>", methods=['PATCH'])
+def Update_status(Data_ID):
+    """[This api is responsible for updating the status in a
+    segnificent row from Our
+    list of data by using the id]
+
+    Returns:
+        [json dictionary]: [It reutrn the result of
+        this api method wether it's a success by
+        returning status 200 and success message
+        or failuer by returning status 404 and an error message]
+    """
+    try:
+        data = request.get_json()
+        db_helper.update_data_status(Data_ID, data["status"])
+        result = {
+            "status": 200,
+            "response": "The status for this data has been updated succesfully"
         }
     except Exception as err:
         result = {
@@ -42,6 +88,18 @@ def update(Data_ID):
 
 @app.route("/api/v1/todo", methods=['POST'])
 def create():
+    """[This api is resbosiable for creating a list
+    of data in our todo list it sends the data
+    into a function in our local database]
+
+    Returns:
+        [Json Dictionary]: [in case of success it returns the
+        result to the user after
+        inserting the data which are the status: 200 and a response aka success
+        message in case of failuer it will return status 404 and a response
+        with an expetion error message]
+    """
+
     data = request.get_json()
     db_helper.insert_data(data['description'])
     try:
@@ -59,6 +117,16 @@ def create():
 
 @app.route("/api/v1/todo/", methods=["GET"])
 def GetAllData():
+    """[This funtcion gets all the data from our
+    todo list it sends the request to a function in our
+    local database to retrive all the data]
+
+    Returns:
+        [Json dictionary]: [in case of success it returns a status of 200
+        and the result which is the data retreived from the list in our local
+        database in case of failuer it will
+        return status 400 and and exception error]
+    """
     try:
         result = db_helper.Get_Data()
         items = {
@@ -74,6 +142,43 @@ def GetAllData():
     return jsonify(items)
 
 
+"""[This funtcion gets a segnificent row of data depending on the id entered]
+
+    Returns:
+        [Json dictionary]: [in case of success it returns a status of 200
+        and the result which is the data retreived that matches the
+        entered id from the list in our local
+        database in case of failuer it will
+        return status 400 and and exception error]
+"""
+
+
+@app.route("/api/v1/todo/<Data_ID>", methods=["GET"])
+def GetDataByID(Data_ID):
+    """[This funtcion gets a segnificent row of data depending on the id entered]
+
+    Returns:
+        [Json dictionary]: [in case of success it returns a status of 200
+        and the result which is the data retreived that matches the
+        entered id from the list in our local
+        database in case of failuer it will
+        return status 400 and and exception error]
+    """
+    try:
+        result = db_helper.GetDataByID(Data_ID)
+        items = {
+            "status": 200,
+            "response": result
+        }
+
+    except Exception:
+        items = {
+            'status': 404,
+            "response": str(Exception)
+        }
+    return jsonify(items)
+
+
 @app.route("/")
 def homepage():
-    items = db_helper.fetch_todo()
+    return 0
